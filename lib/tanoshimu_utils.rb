a0e34bec977bfc2cb37913f40cea9d094f1adddb
Akinyele Cafe-Febrissy <me@akinyele.ca>
2020-01-10 14:17:57 -0500

    Add gem contents

--- lib/tanoshimu_utils.rb
+++ lib/tanoshimu_utils.rb
@@ -1,5 +1,11 @@
-require "tanoshimu_utils/railtie"
+require 'active_support'
+
+require 'tanoshimu_utils/railtie'
+require 'tanoshimu_utils/version'
+require_relative 'tanoshimu_utils/concerns'
+require_relative 'tanoshimu_utils/validators'
 
 module TanoshimuUtils
+  class Error < StandardError; end
   # Your code goes here...
 end
